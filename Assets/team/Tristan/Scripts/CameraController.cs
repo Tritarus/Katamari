beecf6bce69a872cdacd869b9227b6bad85e3f87
Tristan Jardon <tristan.jardon@gmail.com>
2017-12-13 12:05:31 +0100

    Reorganize folderwq

---
+++ Assets/team/Tristan/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Public Members
+
+    public GameObject m_player;
+    
+    #endregion
+
+    #region Public void
+
+    #endregion
+
+    #region System
+
+    void Start()
+    {
+        m_offset = transform.position - m_player.transform.position;
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private an Protected Members
+
+    private vector3 m_offset;
+
+    #endregion
+}
