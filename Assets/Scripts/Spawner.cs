92a337cf739a4f032836a3908f57b5d95bf0bbd4
tristan.jardon <tristan.jardon@gmail.com>
2017-12-25 23:16:40 +0100

    Spawn optimization

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,6 +8,7 @@ public class Spawner : MonoBehaviour
     public GameObject m_zorb;
     public Camera m_camera;
     public int m_ballNumber;
+    public GameObject m_collectible;
     [HideInInspector]
     public ArrayList m_collectibleList = new ArrayList();
     #endregion
@@ -17,11 +18,11 @@ public class Spawner : MonoBehaviour
     private void Awake()
     {
         m_planeRenderer = GetComponentInChildren<Renderer>();
-        m_minusX = -m_planeRenderer.bounds.size.x / 2 + 5;
-        m_plusX = m_planeRenderer.bounds.size.x / 2 - 5;
-        m_minusZ = -m_planeRenderer.bounds.size.z / 2 + 5;
-        m_plusZ = m_planeRenderer.bounds.size.z / 2 - 5;
-        Debug.Log("Spawn Zone : " + m_minusX + "," + m_minusZ + "-" + m_plusX + "," + m_plusZ);
+        m_minusX = -m_planeRenderer.bounds.size.x * 0.5f;
+        m_plusX = m_planeRenderer.bounds.size.x * 0.5f;
+        m_minusZ = -m_planeRenderer.bounds.size.z * 0.5f;
+        m_plusZ = m_planeRenderer.bounds.size.z * 0.5f;
+        Debug.Log("Spawn Zone : " + m_minusX + "," + m_minusZ + ":" + m_plusX + "," + m_plusZ);
     }
 
     void OnEnable()
@@ -32,11 +33,12 @@ public class Spawner : MonoBehaviour
             Spawn(.8f, init: true);
             Spawn(1f, init: true);
         }
+        m_minusX = -m_planeRenderer.bounds.size.x * 0.45f;
+        m_plusX = m_planeRenderer.bounds.size.x * 0.45f;
+        m_minusZ = -m_planeRenderer.bounds.size.z * 0.45f;
+        m_plusZ = m_planeRenderer.bounds.size.z * 0.45f;
         //Debug.Log("Collectible number : " + m_collectibleList.Count);
     }
-
-
-    //Debug.Log("T(): );
 
     #endregion
 
@@ -70,35 +72,23 @@ public class Spawner : MonoBehaviour
     }
     private void SpawnV2(float radiusModifier = 1.2f, bool init = false)
     {
-        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-
         Vector3 worldVect = Vector3.zero;
         Vector3 camVect = Vector3.zero;
-        bool outOfCamera = false;
+        bool collectibleDropped = false;
         int maxTest = 5;
-        for (int i = 0; i <= maxTest && !outOfCamera; i++)
+        for (int i = 0; !collectibleDropped && i <= maxTest ; i++)
         {
-            worldVect = new Vector3(Random.Range(m_minusX, m_plusX), sphere.GetComponent<SphereCollider>().radius, Random.Range(m_minusZ, m_plusZ));
-            camVect = m_camera.WorldToViewportPoint(worldVect);
-            if (camVect.x < -1 || camVect.x > 1 || camVect.y < -1 || camVect.y > 1 || init)
+            worldVect = new Vector3(Random.Range(m_minusX, m_plusX), 0.1f, Random.Range(m_minusZ, m_plusZ));
+            if(!init) camVect = m_camera.WorldToViewportPoint(worldVect);
+            if (init || camVect.x < -1.05 || camVect.x > 1.05 || camVect.y < -1.05 || camVect.y > 1.05)
             {
-                sphere.layer = 9;
-                sphere.AddComponent<Rigidbody>();
-                sphere.GetComponent<Rigidbody>().mass = 0;
+                GameObject sphere = Instantiate(m_collectible);
+                if (!init) sphere.GetComponent<MeshRenderer>().material.color = Color.red;
                 sphere.transform.localScale = GetScale(m_zorb, radiusModifier);
                 sphere.transform.position = worldVect;
                 m_collectibleList.Add(sphere);
-                outOfCamera = true;
+                collectibleDropped = true;
             }
-            else if(i == maxTest)
-            {
-                Destroy(sphere);
-                //Debug.Log(m_collectibleList.Count);
-            }
-        }
-        if (!init)
-        {
-            sphere.GetComponent<MeshRenderer>().material.color = Color.red;
         }
     }
 
